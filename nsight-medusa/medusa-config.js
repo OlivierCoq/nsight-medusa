88ff87a8165062da99d7ea4c4fa878716a655a27
OlivierC <coq.olivier36@gmail.com>
2023-12-25 19:55:32 -0500

    Adjusting for CORS

--- nsight-medusa/medusa-config.js
+++ nsight-medusa/medusa-config.js
@@ -19,47 +19,23 @@ switch (process.env.NODE_ENV) {
 
 try {
   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
-} catch (e) { }
+} catch (e) {}
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001,http://localhost:3000";
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000,http://localhost:3000";
-
-
-/* DIGITAL OCEAN:
-  We'll switch between the two depending on the environment
-*/
-// production:
-const DB_USERNAME = process.env.DB_USERNAME
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_HOST = process.env.DB_HOST
-const DB_PORT = process.env.DB_PORT
-const DB_DATABASE = process.env.DB_DATABASE
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 const DATABASE_URL =
-  `postgres://${DB_USERNAME}:${DB_PASSWORD}` +
-  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}` || "postgres://localhost/medusa-starter-default";
-
-// local:
-// const DATABASE_URL =
-//   process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
+  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
-  {
-    resolve: "medusa-plugin-sendgrid",
-    options: {
-      api_key: process.env.SENDGRID_API_KEY,
-      from: process.env.SENDGRID_SENDER,
-      order_placed_template: "d-7fce8d2bea4942789d6c80d5f8fb3a33"
-    }
-  },
   {
     resolve: `@medusajs/file-local`,
     options: {
@@ -100,13 +76,8 @@ const projectConfig = {
   store_cors: STORE_CORS,
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
-  database_extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
   // Uncomment the following lines to enable REDIS
-  redis_url: REDIS_URL || process.env.REDIS_URL
+  // redis_url: REDIS_URL
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
